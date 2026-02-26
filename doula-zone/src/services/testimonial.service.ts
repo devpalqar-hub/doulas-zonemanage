f5021c2da2e64f3d44f55618b0ed40f9bbfa4235
mudfayaz44 <muhdfayaz44@gmail.com>
2026-02-26 14:07:47 +0530

    Testimonnial api connected

--- doula-zone/src/services/testimonial.service.ts
+++ doula-zone/src/services/testimonial.service.ts
@@ -10,9 +10,45 @@ export type Testimonial = {
   createdAt: string;
 };
 
-export const fetchTestimonials = async () => {
-  const res = await api.get("/testimonials");
-  return res.data.data as Testimonial[];
+type FetchTestimonialsParams = {
+  page: number;
+  limit: number;
+  serviceName?: string;
+  ratings?: number;
+  startDate?: string;
+  endDate?: string;
+};
+
+export const fetchTestimonials = async (
+  filters: FetchTestimonialsParams
+) => {
+  const params: any = {
+    page: filters.page,
+    limit: filters.limit,
+  };
+
+  if (filters.serviceName) params.serviceName = filters.serviceName;
+  if (filters.ratings) params.ratings = filters.ratings;
+  if (filters.startDate) params.startDate = filters.startDate;
+  if (filters.endDate) params.endDate = filters.endDate;
+
+  const res = await api.get(
+    "/testimonials/all/testimonials",
+    { params }
+  );
+
+  return {
+    data: res.data.data.map((t: any) => ({
+      id: t.servicePricingId + "-" + t.testimonialCreatedAt, 
+      ratings: t.ratings,
+      reviews: t.reviews,
+      doulaName: t.doulaName,
+      serviceName: t.serviceName,
+      clientName: t.clientName,
+      createdAt: t.testimonialCreatedAt,
+    })) as Testimonial[],
+    meta: res.data.meta,
+  };
 };
 
 export const fetchTestimonialById = async (id: string) => {
